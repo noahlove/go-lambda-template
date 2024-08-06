10f0bcae65f398011c90ebcb05562071a1a1c6c5
Noah Love <noah.love@columbia.edu>
2024-08-05 20:38:04 -0600

    Parameterized execute

--- cmd/execute/main.go
+++ cmd/execute/main.go
@@ -6,42 +6,68 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"gopkg.in/yaml.v2"
 )
 
-const (
-	region       = "us-west-2"
-	functionName = "hello-world-lambda"
-	awsProfile   = "personal"
-)
+type Config struct {
+	AWS struct {
+		Region  string `yaml:"region"`
+		Profile string `yaml:"profile"`
+	} `yaml:"aws"`
+	Lambda struct {
+		FunctionName string `yaml:"function_name"`
+	} `yaml:"lambda"`
+}
 
 type LambdaEvent struct {
 	Name string `json:"name"`
 }
 
+func loadConfig(filename string) (Config, error) {
+	var cfg Config
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return cfg, fmt.Errorf("error reading config file: %v", err)
+	}
+
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return cfg, fmt.Errorf("error parsing config file: %v", err)
+	}
+
+	return cfg, nil
+}
+
 func main() {
+	// Load configuration
+	cfg, err := loadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
 	// Parse command-line arguments
 	name := flag.String("name", "", "Name to pass to the Lambda function")
 	flag.Parse()
-
 	if *name == "" {
 		log.Fatal("Name is required. Use -name flag to provide a name.")
 	}
 
 	// Load AWS configuration
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion(region),
-		config.WithSharedConfigProfile(awsProfile),
+	awsCfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(cfg.AWS.Region),
+		config.WithSharedConfigProfile(cfg.AWS.Profile),
 	)
 	if err != nil {
 		log.Fatalf("Unable to load SDK config: %v", err)
 	}
 
 	// Create Lambda client
-	client := lambda.NewFromConfig(cfg)
+	client := lambda.NewFromConfig(awsCfg)
 
 	// Prepare the Lambda event
 	event := LambdaEvent{
@@ -56,7 +82,7 @@ func main() {
 
 	// Invoke Lambda function
 	result, err := client.Invoke(context.TODO(), &lambda.InvokeInput{
-		FunctionName: aws.String(functionName),
+		FunctionName: aws.String(cfg.Lambda.FunctionName),
 		Payload:      payload,
 	})
 	if err != nil {
